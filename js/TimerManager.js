03e65a12e5fddc4f2822d6fd7673657619a5ec2a
hilroubigza <hilrou.bigza@gmail.com>
2026-06-14 21:41:35 +0700

    Update TimerManager.js

--- js/TimerManager.js
+++ js/TimerManager.js
@@ -2,30 +2,65 @@ class TimerManager{
 
 constructor(seconds){
 
-this.maxTime =
-seconds;
+this.maxTime = seconds;
 
-this.timeLeft =
-seconds;
+this.timeLeft = seconds;
+
+this.running = false;
+
+this.lastTime = 0;
 
 }
 
 start(){
 
-this.timeLeft =
-this.maxTime;
+this.timeLeft = this.maxTime;
+
+this.running = true;
+
+this.lastTime = performance.now();
 
 }
 
-update(delta){
+stop(){
+
+this.running = false;
+
+}
+
+update(){
+
+if(!this.running) return;
+
+const now = performance.now();
+
+const delta =
+
+(now - this.lastTime) / 1000;
+
+this.lastTime = now;
 
 this.timeLeft -= delta;
+
+if(this.timeLeft < 0){
+
+this.timeLeft = 0;
+
+}
 
 }
 
 isFinished(){
 
 return this.timeLeft <= 0;
+
+}
+
+getDisplay(){
+
+return Math.ceil(
+this.timeLeft
+);
 
 }
 
