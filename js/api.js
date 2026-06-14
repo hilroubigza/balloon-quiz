f7176fe8e445dfbec5795f6b252184da723e139f
hilroubigza <hilrou.bigza@gmail.com>
2026-06-14 18:41:37 +0700

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,19 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbx95vZv_oWvntl8ozLCbM3uFCIErB37b6YlUOxi9f6uc6DW2rVyOxAaJgoDe5wLBw30/exec";
+
+async function loadQuestions(){
+
+const response =
+
+await fetch(
+
+`${API_URL}?mode=questions`
+
+);
+
+const data =
+
+await response.json();
+
+return data;
+
+}
