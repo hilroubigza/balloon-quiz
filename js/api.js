50467c34b2c226a0d2042a3c7ed16ff70c1fdb79
hilroubigza <hilrou.bigza@gmail.com>
2026-06-14 18:57:12 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbx95vZv_oWvntl8ozLCbM3uFCIErB37b6YlUOxi9f6uc6DW2rVyOxAaJgoDe5wLBw30/exec";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbx95vZv_oWvntl8ozLCbM3uFCIErB37b6YlUOxi9f6uc6DW2rVyOxAaJgoDe5wLBw30/exec";
 
 async function loadQuestions(){
 
@@ -10,10 +11,56 @@ await fetch(
 
 );
 
-const data =
+return await response.json();
 
-await response.json();
+}
 
-return data;
+async function saveScore(
+name,
+score
+){
+
+return fetch(
+
+API_URL,
+
+{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":
+"application/json"
+
+},
+
+body:JSON.stringify({
+
+mode:"saveScore",
+
+name:name,
+
+score:score
+
+})
+
+}
+
+);
+
+}
+
+async function getLeaderboard(){
+
+const response =
+
+await fetch(
+
+`${API_URL}?mode=leaderboard`
+
+);
+
+return await response.json();
 
 }
