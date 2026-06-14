5b5600cf12913718114a71271272455e182b0996
hilroubigza <hilrou.bigza@gmail.com>
2026-06-14 16:22:47 +0700

    Create Utils.js

---
+++ js/Utils.js
@@ -0,0 +1,36 @@
+function randomInt(
+min,
+max
+){
+
+return Math.floor(
+
+Math.random()*
+(max-min+1)
+
+)+min;
+
+}
+
+function shuffle(arr){
+
+for(
+let i=arr.length-1;
+i>0;
+i--
+){
+
+const j=
+Math.floor(
+Math.random()*(i+1)
+);
+
+[arr[i],arr[j]]
+=
+[arr[j],arr[i]];
+
+}
+
+return arr;
+
+}
